9c85d92e244121cb644e80e0eee99e4fbf66e220
ismael kimutai <ismaelkim2@gmail.com>
2024-11-30 09:53:28 +0300

    scheduled egg archived achieved

--- src/main/java/com/kimsreviews/API/ApiApplication.java
+++ src/main/java/com/kimsreviews/API/ApiApplication.java
@@ -2,8 +2,10 @@ package com.kimsreviews.API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.kimsreviews.API")
+@EnableScheduling
 public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
--- src/main/java/com/kimsreviews/API/scheduling/EggArchieved.java
+++ src/main/java/com/kimsreviews/API/scheduling/EggArchieved.java
@@ -3,16 +3,17 @@ package com.kimsreviews.API.scheduling;
 import com.kimsreviews.API.Services.EggsRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component // Ensures Spring detects and manages this class
 public class EggArchieved {
 
     @Autowired
     private EggsRecordService eggsRecordService;
 
     // Run every Sunday at midnight
-    @Scheduled(cron = "0 0 0 * * SUN")
+    @Scheduled(cron = "0 0 0 ? * SUN") // Correct cron expression
     public void archiveWeeklyRecords() {
         eggsRecordService.archiveCurrentWeekData();
     }
-
 }
