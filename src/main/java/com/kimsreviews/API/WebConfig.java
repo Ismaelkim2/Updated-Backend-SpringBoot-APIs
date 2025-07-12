be0f1f7c538b0f3d44f59bdd233db0c4c87afe64
ismael kimutai <ismaelkim2@gmail.com>
2025-07-12 20:19:03 +0300

    configured

---
+++ src/main/java/com/kimsreviews/API/WebConfig.java
@@ -0,0 +1,27 @@
+package com.kimsreviews.API;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
+
+import static org.springframework.http.CacheControl.maxAge;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://dashing-squirrel-91b6d0.netlify.app") // your frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
